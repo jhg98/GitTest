df3836a20f994868c1b17a407bdd485a3f0b466a
jhg <jppp5@naver.com>
2025-04-17 14:43:51 +0900

    초기 커밋.

---
+++ 20250417_GitTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _20250417_GitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
